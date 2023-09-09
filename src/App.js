f711e63aa2b7e6409936e22a3a7fb33abf83f212
gurung69 <gurungkrish44@gmail.com>
2023-09-09 11:51:31 -0600

    Updated Readme file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { Form } from './components/Form/Form';
 import { Info } from './components/Info/Info';
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [location, setLocation] = useState('');
+
+
+  async function weather(location){
+    const key = ""; // open weather api key
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+key+"&units=metric";
+    //fetch data
+    try{
+      const response = await axios.get(url)
+      setWeatherInfo(response.data)
+
+      setLocation(location)
+    } 
+    catch(error){
+      console.log(error);
+    };
+  };
+
+  // get users current location weather
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude
+      const lng = position.coords.longitude
+      
+      // get user's city name
+      const key = "" // api key from Open cage
+      const url = "https://api.opencagedata.com/geocode/v1/json?q=+"+lat+"+"+lng+"&key="+key;
+      try{
+        axios.get(url)
+        .then(response=>{
+          setLocation(response.data.results[0].components.city)
+          weather(response.data.results[0].components.city)
+  
+        })
+      }
+      catch(error){
+        console.log(error)
+      }
+    });
+  }, []);
+  
+
   return (
     <div className="App">
-      <Form />
-      <Info />
+      <Form 
+      fetchWeather={weather}
+      />
+      <Info
+      location={location}
+      weatherInfo={weatherInfo}
+      />
     </div>
   );
 }
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,16 +1,24 @@
 import React from "react";
 import './Info.css'
 
-export function Info(){
+export function Info(props){
+  
+  if (props.weatherInfo == null){
+    return null
+  }
+
+  const {main, weather} = props.weatherInfo;
+
+  
   return (
     <div className="info">
-      <div className="city">Saskatoon</div>       
+      <div className="city">{props.location}</div>       
       <div className="temp-div">
-        <img alt="weather icon" src="https://openweathermap.org/img/wn/11d@2x.png"></img>
-        <span className="temp">23</span>&nbsp;°C
+        <img alt="weather icon" src={"https://openweathermap.org/img/wn/"+weather[0].icon+"@2x.png"}></img>
+        <span className="temp">{main.temp}</span>&nbsp;°C
       </div>
-      <div>Humidity: <span className="humidity">8</span>&nbsp;%</div>
-      <div>Feels like: <span className="feels-like">23</span>°C</div>
+      <div>Humidity: <span className="humidity">{main.humidity}</span>&nbsp;%</div>
+      <div>Feels like: <span className="feels-like">{main.feels_like}</span>°C</div>
     </div>
   )
 } 
